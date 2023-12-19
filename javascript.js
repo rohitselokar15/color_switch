ab5ac93c14bfc473eb188104de1966e9a6c6462a
rohitselokar15 <rohitselokar1572002@gmail.com>
2023-12-19 17:50:55 +0530

    initial file

---
+++ javascript.js
@@ -0,0 +1,17 @@
+function changeBg(color){
+    document.body.style.background = color;
+
+    let txt = document.getElementsByClassName("text");
+    if(color == '#000000'){
+        for(let elm of txt){
+            elm.style.color = "white";
+        }
+    }
+    else{
+        for(let elm of txt){
+            elm.style.color = "black";
+        }
+    }
+}
+
+
